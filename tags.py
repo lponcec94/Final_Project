136ac8f604382a5073daa3830815dd4c6ef65e90
lponcec94 <lponcec94@gmail.com>
2019-09-05 16:25:58 -0500

    archivo con etiquetas

---
+++ tags.py
@@ -0,0 +1,7 @@
+authors_tag = ['hume', 'plato', 'voltaire', 'russell', 'kant']
+
+text_titles = ['Principles_of_morals_Hume', 'Political_discourses_Hume', 'Dialogues_natural_religion_Hume','Concerning_human_understanding_Hume',
+'Cratylus_Plato', 'Apology_criton_phaedo_Plato', 'Gorgias_Plato', 'Republic_Plato',
+ 'History_peter_great_Voltaire', 'Socrates_Voltaire', 'Philosophical_dictionary_Voltaire', 'Candide_Voltaire',
+ 'Analysis_Mind_Russell', 'Mysticism_logic_Russell', 'Problems_philosophy_Russell', 'Roads_freedom_Russell', 
+ 'Pure_reason_Kant', 'Practical_reason_Kant', 'Judgment_Kant', 'Perpetual_peace_Kant']
